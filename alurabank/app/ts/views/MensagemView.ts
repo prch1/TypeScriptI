9653429331b25aee0f654d24a823f1d4e33f9101
Paulo Ricardo <pauloricardoh@hotmail.com>
2021-03-06 12:33:00 -0300

    Aula 4 - Generic

--- alurabank/app/ts/views/MensagemView.ts
+++ alurabank/app/ts/views/MensagemView.ts
@@ -1,12 +1,6 @@
 
-class MensagemView extends View {
+class MensagemView extends View<string> {
   
-
-    update(model : string)
-    {
-        this._elemento.innerHTML = this.template(model);
-    }
-
     template(model:string) : string {
 
         return `<p class="alert alert-info">${model}</p>`
--- alurabank/app/ts/views/NegociacoesView.ts
+++ alurabank/app/ts/views/NegociacoesView.ts
@@ -1,11 +1,5 @@
 
-
-class NegociacoesView extends View{
-
-
-     update(model : Negociacoes): void {
-         this._elemento.innerHTML = this.template(model);
-     }
+class NegociacoesView extends View<Negociacoes>{
 
      template(model : Negociacoes): string {
          return `
